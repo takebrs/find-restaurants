208a81d005739b9a025c1446335f493dbf17f326
Kondo Tomoka <taketomo@KondonoMacBook-Pro.local>
2025-06-01 02:40:46 +0900

    i solve cloud issue

--- cf-worker-proxy/index.js
+++ cf-worker-proxy/index.js
@@ -1,10 +1,10 @@
 export default {
-    async fetch(request) {
+    async fetch(request, env) {
       const url = new URL(request.url);
       const q = url.searchParams.get("q") || "";
   
       const apiUrl = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/` +
-        `?key=${HOTPEPPER_KEY}&keyword=${encodeURIComponent(q)}&format=json&count=20`;
+        `?key=${env.HOTPEPPER_KEY}&keyword=${encodeURIComponent(q)}&format=json&count=20`;
   
       const res = await fetch(apiUrl);
   
